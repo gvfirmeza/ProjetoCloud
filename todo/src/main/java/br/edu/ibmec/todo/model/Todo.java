b254a230da53a0b4de156bb785e6e9755751863f
GUILHERME VIEIRA FIRMEZA <202301135737@alunos.ibmec.edu.br>
2024-09-02 09:01:05 -0300

    feat: validation

--- todo/src/main/java/br/edu/ibmec/todo/model/Todo.java
+++ todo/src/main/java/br/edu/ibmec/todo/model/Todo.java
@@ -1,18 +1,24 @@
 package br.edu.ibmec.todo.model;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class Todo {
 
+    @NotBlank(message = "Campo id obrigatório")
     private int id;
 
+    @NotBlank(message = "Campo name obrigatório")
     private String name;
 
+    @NotBlank(message = "Campo owner obrigatório")
     private String owner;
 
+    @NotBlank(message = "Campo status obrigatório")
     private String status;
 
+    @NotBlank(message = "Campo description obrigatório")
     private String description;
 
 }
