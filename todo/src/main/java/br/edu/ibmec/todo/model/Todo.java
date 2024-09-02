5f003c15d0282409084a8c88fea686b36c8ca29c
gvfirmeza <gvfirmeza@gmail.com>
2024-09-02 15:11:53 -0300

    fix: validation

--- todo/src/main/java/br/edu/ibmec/todo/model/Todo.java
+++ todo/src/main/java/br/edu/ibmec/todo/model/Todo.java
@@ -1,12 +1,14 @@
 package br.edu.ibmec.todo.model;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class Todo {
 
-    @NotBlank(message = "Campo id obrigatório")
+    @NotNull(message = "Campo id obrigatório")
     private int id;
 
     @NotBlank(message = "Campo name obrigatório")
