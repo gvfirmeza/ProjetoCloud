2540d44d4038cc2a006aaf4a97a16c5c37103323
GUILHERME VIEIRA FIRMEZA <202301135737@alunos.ibmec.edu.br>
2024-09-02 08:48:43 -0300

    adding todo project

---
+++ todo/src/main/java/br/edu/ibmec/todo/model/Todo.java
@@ -0,0 +1,18 @@
+package br.edu.ibmec.todo.model;
+
+import lombok.Data;
+
+@Data
+public class Todo {
+
+    private int id;
+
+    private String name;
+
+    private String owner;
+
+    private String status;
+
+    private String description;
+
+}
